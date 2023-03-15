bdd862e388b885d27939f1e967d916503d91ebdf
root <root@LAPTOP-6B2EVI76.localdomain>
2023-03-15 17:38:15 -0400

    Added comments

--- spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
+++ spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
@@ -21,6 +21,8 @@ public class SpamResource {
 
 //    your SpamDetector Class responsible for all the SpamDetecting logic
     SpamDetector detector = new SpamDetector();
+
+    //Json Mapper declaration
     ObjectMapper jsonMapper = new ObjectMapper();
 
     List<TestFile> testList = null;
@@ -31,12 +33,15 @@ public class SpamResource {
     private int falseNegatives = 0;
 
 
+    //Function Runs in background upon startup
     SpamResource() throws FileNotFoundException {
 //        TODO: load resources, train and test to improve performance on the endpoint calls
         System.out.print("Training and testing the model, please wait");
 
+        //Populate the testList
         testList = trainAndTest();
 
+        //Evaluate the model based on the testList
         APCalc();
 
         System.out.print("Breakpoint here");
@@ -56,6 +61,7 @@ public class SpamResource {
         return res;
     }
 
+    //Return testList as json response object
     @GET
     @Path("/json")
     @Produces("application/json")
@@ -64,8 +70,9 @@ public class SpamResource {
 
         String json = null;
 
+        //Serialize testList using the JsonMapper
         try{
-            json = jsonMapper.writeValueAsString(testList);
+            json = jsonMapper.writeValueAsString(testList); //Return a string representation of the json objects
         }catch (JsonProcessingException e){
             e.printStackTrace();
         }
@@ -79,10 +86,14 @@ public class SpamResource {
 
     }
 
+    //Caculate the number of true positives, true negatives, false positives and false negatives contained within
+    //testList
     private void APCalc(){
 
         for (TestFile file:  testList){
 
+            //If a TestFile object has a spamProbability value greater than 50%, and it's actual class is spam,
+            //then it is a true positive. If its actual class is not spam, then it's a false positive
             if ( file.getSpamProbability() > 0.50){
                 if (file.getActualClass() == "spam"){
                     truePositives++;
@@ -91,6 +102,8 @@ public class SpamResource {
                     falsePositives++;
                 }
             }
+            //If a TestFile object has a spamProbability value less than 50%, and it's actual class is ham,
+            //then it is a true negative. It its actual class is not ham, then it's a false negative
             else{
                 if (file.getActualClass() == "ham"){
                     trueNegatives++;
@@ -104,6 +117,7 @@ public class SpamResource {
 
     }
 
+    //Internal class that represents the accuracy of the model
     class Accuracy{
         int TruePositives;
         int TrueNegatives;
@@ -130,14 +144,18 @@ public class SpamResource {
 
     }
 
+    //Returns an accuracy object as a json response object, containing information about the model's accuracy, the number of true positives,
+    //and the number of true negatives
     @GET
     @Path("/accuracy")
     @Produces("application/json")
     public Response getAccuracy() {
 //      TODO: return the accuracy of the detector, return in a Response object
 
+        //Calculate the percentage accuracy of the model
         double accuracyPercent = (double) (truePositives + trueNegatives)/(testList.size());
 
+        //Create the accuracy object
         Accuracy accuracy = new Accuracy(truePositives, trueNegatives, accuracyPercent);
 
         String accuracyJson = null;
@@ -157,18 +175,24 @@ public class SpamResource {
         return accuracyResp;
     }
 
+    //Internal class that represents the precision of the model
     class Precision{
 
         int TruePositives;
         int FalsePositives;
 
+        int FalseNegatives;
+
         double PrecisionPercent;
 
-        Precision(int TruePositives, int FalsePositives, double PrecisionPercent){
+        Precision(int TruePositives, int FalsePositives, int FalseNegatives, double PrecisionPercent){
             this.TruePositives = TruePositives;
             this.FalsePositives = FalsePositives;
             this.PrecisionPercent = PrecisionPercent;
+            this.FalseNegatives = FalseNegatives;
         }
+
+        //Set getter functions to allow for serialization of the fields
 
         public int getTruePositives(){
             return  this.TruePositives;
@@ -178,13 +202,16 @@ public class SpamResource {
             return this.FalsePositives;
         }
 
+        public int getFalseNegatives(){return this.FalseNegatives;}
+
         public double getPrecisionPercent(){
             return PrecisionPercent;
         }
 
-
     }
 
+    //Returns a precision object as a json response object, containing information about the model's precision, the number of true positives,
+    //and the number of false positives
     @GET
     @Path("/precision")
     @Produces("application/json")
@@ -193,7 +220,7 @@ public class SpamResource {
 
         double precisionPercent = (double) (truePositives)/(falsePositives + truePositives);
 
-        Precision precision = new Precision(truePositives, falsePositives, precisionPercent);
+        Precision precision = new Precision(truePositives, falsePositives, falseNegatives, precisionPercent);
 
         String precisionJson = null;
 
--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -13,6 +13,8 @@ import java.util.*;
  */
 public class SpamDetector {
 
+    //Custom Comparator used in the sorting of the TestFile list
+    //Comparator sorts TestFiles from least to greatest based on the spamProabability field
     class spamProbabilityComparator  implements Comparator<TestFile>{
 
         public int compare(TestFile file1, TestFile file2){
@@ -55,7 +57,8 @@ public class SpamDetector {
         //Analyze frequency of spam files containing a word
         int spam_size = countInstance(trainSpamFreq, spam);
 
-
+        //For any word in the ham word map that's not in the spam word map, add to the spam map a (key,value) pair
+        //consisting of the missing word and 0
         for (String key: trainHamFreq.keySet()){
 
             if (!trainSpamFreq.containsKey(key)){
@@ -63,6 +66,8 @@ public class SpamDetector {
             }
         }
 
+        //For any word in the spam word map that's not in the ham word map, add to the ham map a (key,value) pair
+        //consisting of the missing word and 0
         for (String key: trainSpamFreq.keySet()){
 
             if (!trainHamFreq.containsKey(key)){
@@ -70,23 +75,28 @@ public class SpamDetector {
             }
         }
 
+        //Create a probability map consisting of <word, % chance of being spam> pairs
         Map<String, Double> probMap = createProbabilityMap(trainHamFreq, trainSpamFreq, ham_size, spam_size);
 
+        //Access test files
         File spam_test = new File(mainDirectory, "test/spam");
         File ham_test = new File(mainDirectory, "test/ham");
 
+        //Declare ArrayList that will be returned
         ArrayList<TestFile> testList = new ArrayList<>();
 
-        TestTestFileList(spam_test, "spam", probMap, testList);
-        TestTestFileList(ham_test, "ham", probMap, testList);
+        //Evaluate the files contained within the test directories and add them to testList
+        EvalTestFileList(spam_test, "spam", probMap, testList);
+        EvalTestFileList(ham_test, "ham", probMap, testList);
 
+        //Sort the test list from least to greatest in term of the probability that a given file is spam
         Collections.sort(testList, new spamProbabilityComparator());
 
         return testList;
     }
 
     //Modifies a wordMap to create entries <Word, frequency> where frequency is the number of files containing
-    //that word
+    //that word. Returns the number of files contained within the directory passed as argument
     private int countInstance(Map<String, Integer> wordMap, File file) throws FileNotFoundException {
 
         //Create list of email files in the directory
@@ -109,6 +119,7 @@ public class SpamDetector {
                     //Convert all words to lower case
                     String word = (scanner.next()).toLowerCase();
 
+                    //Ensure that word being parsed contains only letter characters
                     if (isWord(word)){
 
                         //Add word to temp map if it doesn't exist already
@@ -144,10 +155,12 @@ public class SpamDetector {
 
     }
 
+    //Creates a probability map that maps a word to the probability that it is contained within a spam file
     private Map<String, Double> createProbabilityMap(Map<String, Integer> ham, Map<String, Integer> spam, int ham_size, int spam_size){
 
         Map<String, Double> probMap = new TreeMap<>();
 
+        //Maps should be the same size and contain the same keys after their contents were merged
         for (String key: spam.keySet()){
 
             double probWiS = ((double) spam.get(key))/(spam_size);
@@ -162,8 +175,10 @@ public class SpamDetector {
         return probMap;
     }
 
-    private void TestTestFileList(File dir, String actualClass, Map<String, Double> probMap, ArrayList<TestFile> fileList) throws FileNotFoundException {
+    //Evaluate the probability that a test file is spam
+    private void EvalTestFileList(File dir, String actualClass, Map<String, Double> probMap, ArrayList<TestFile> fileList) throws FileNotFoundException {
 
+        //Access the files within the directory
         File[] testFiles = dir.listFiles();
 
         for (File testFile: testFiles){
@@ -179,12 +194,13 @@ public class SpamDetector {
                     //Convert all words to lower case
                     String word = (scanner.next()).toLowerCase();
 
+                    //Check if word contains only letters
                     if (isWord(word)){
 
+
                         if (probMap.containsKey(word)){
-                            //System.out.println(probMap.get(word));
 
-
+                            //If the probability that the word is contained in spam file is greater than 0, use it to evaluate nu
                             if ((probMap.get(word) > 0) && probMap.get(word) < 1){
                                 nu += Math.log(1- probMap.get(word)) - Math.log(probMap.get(word));
                             }
@@ -195,6 +211,7 @@ public class SpamDetector {
                     }
                 }
 
+                //Calculate the probability based on the calculated nu value
                 double probSF = 1/(1 + Math.pow(Math.E,nu));
 
                 TestFile testFileObject = new TestFile(testFile.getName(),probSF, actualClass);
